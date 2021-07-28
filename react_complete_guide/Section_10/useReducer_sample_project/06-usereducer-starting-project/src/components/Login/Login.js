7aef041696cd9ccd2e8c64b729bd302e509862ed
gabi9301 <dbwksl128@naver.com>
2021-07-28 17:49:13 +0900

    react_useReducer updated

--- react_complete_guide/Section_10/useReducer_sample_project/06-usereducer-starting-project/src/components/Login/Login.js
+++ react_complete_guide/Section_10/useReducer_sample_project/06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,15 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+
+const emailReducer = (state, action) => {
+  if(action.type === 'USER_INPUT') {
+    return {value:'', isValid: action.val.includes('@')};
+  }
+
+  if(action.type === 'INPUT_BLUR') {
+    return {value: state.value, isValid: state.value.includes('@')};
+  }
+  
+  return {value: '', isValid: false};
+
+};  
+//reducer 함수를 컴포넌트 함수 밖에서 선언할 수 있는 이유는 컴포넌트 함수로 생겨난 
+//어떠한 데이터도 필요로 하지 않기 때문이다.
+//reducer 함수가 필요로 하는 데이터는 해당 함수를 실행할 때 리액트가 자동으로 넘겨준다.
+
+
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: false});
+
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -34,7 +56,7 @@ const Login = (props) => {
   // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type:'USER_INPUT', val : event.target.value});
 
     setFormIsValid(
       event.target.value.includes('@') && enteredPassword.trim().length > 6
@@ -45,12 +67,12 @@ const Login = (props) => {
     setEnteredPassword(event.target.value);
 
     setFormIsValid(
-      enteredEmail.includes('@') && event.target.value.trim().length > 6
+      emailState.isValid && event.target.value.trim().length > 6
     );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
@@ -59,7 +81,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -67,14 +89,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
