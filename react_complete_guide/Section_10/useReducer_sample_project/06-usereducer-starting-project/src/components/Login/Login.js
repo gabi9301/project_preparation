e1a771769eeda312563b78bb6d694b97d5d3337f
gabi9301 <dbwksl128@naver.com>
2021-08-02 23:06:41 +0900

    Dynamic Context updated

--- react_complete_guide/Section_10/useReducer_sample_project/06-usereducer-starting-project/src/components/Login/Login.js
+++ react_complete_guide/Section_10/useReducer_sample_project/06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 
 const emailReducer = (state, action) => {
@@ -46,6 +47,8 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null});
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: null});
+
+  const authCtx = useContext(AuthContext);
 
 
   const {isValid: emailIsValid} = emailState;
@@ -99,7 +102,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
