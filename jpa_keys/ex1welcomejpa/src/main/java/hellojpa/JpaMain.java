795c135bf87422fdb7894086bb3d52f7e06518a2
gabi9301 <dbwksl128@naver.com>
2021-08-05 20:03:26 +0900

    relationship mapping, react app updated

--- jpa_keys/ex1welcomejpa/src/main/java/hellojpa/JpaMain.java
+++ jpa_keys/ex1welcomejpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -30,11 +31,16 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
 
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();                                //트랜젝션을 커밋
 
