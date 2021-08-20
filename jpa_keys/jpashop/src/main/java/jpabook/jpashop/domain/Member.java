27a693b3eab26194e877a359375569473e2a7526
gabi9301 <dbwksl128@naver.com>
2021-08-20 20:52:42 +0900

    relationship mapping, react practice updated 8/20

--- jpa_keys/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa_keys/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,6 +15,10 @@ public class Member {
     private String street;
     private String zipcode;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
