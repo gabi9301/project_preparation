0be6326ccad52b3cab9b21c74de7694361b52be2
yjlee <yj.lee@gtradepay.com>
2021-09-09 15:47:06 +0900

    value type, react http 9/9 updated

--- jpa_keys/ex1welcomejpa/src/main/java/hellojpa/ValueMain.java
+++ jpa_keys/ex1welcomejpa/src/main/java/hellojpa/ValueMain.java
@@ -7,11 +7,25 @@ public class ValueMain {
 //
 //        a = 20;
 
-        Integer a = new Integer(10);
-        Integer b = a;
+//        Integer a = new Integer(10);
+//        Integer b = a;
+//
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int a = 10;
+        int b = 10;
+
+        System.out.println("a == b: " + (a == b));
+
+        Address address1 = new Address("city","street","10000");
+        Address address2 = new Address("city","street","10000");
+
+        System.out.println("address1 == address2: " + (address1 == address2));
+        System.out.println("address1 equals address2: " + (address1.equals(address2)));
+
+
+
 
     }
 }
