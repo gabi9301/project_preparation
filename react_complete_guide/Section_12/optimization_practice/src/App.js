d95f1fe09c0233005086da9925ff8db3d96a16ff
yjlee <yj.lee@gtradepay.com>
2021-09-13 18:06:25 +0900

    value type, react optimization 9/13 updated

--- react_complete_guide/Section_12/optimization_practice/src/App.js
+++ react_complete_guide/Section_12/optimization_practice/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App Running!!!");
+
+  const toggleButtonHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph); 
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+     {showParagraph && <p>This is new!!</p>}
+      <Button onClick={toggleButtonHandler}>Toggle Paragraph!</Button>
     </div>
+
   );
 }
 
