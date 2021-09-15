d20e80acda3a37a8ef6654b6ec2660126e7ab8e0
yjlee <yj.lee@gtradepay.com>
2021-09-15 18:02:38 +0900

    JPQL, react optimization 9/15 updated

---
+++ jpa_keys/jpql/src/main/java/jpql/JpqlMain.java
@@ -0,0 +1,36 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpqlMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query.getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
